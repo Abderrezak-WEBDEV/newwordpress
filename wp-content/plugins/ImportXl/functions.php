8ad16dfe768adf4c51fcf172c4d44425c1d47be6
AbderrezakWEBDEV <razik.bouiche@gmail.com>
2022-06-14 11:52:37 +0200

    commit 5 nouvelle extention

--- wp-content/plugins/ImportXl/functions.php
+++ wp-content/plugins/ImportXl/functions.php
@@ -8,34 +8,33 @@ function wpm_custom_post_type() {
 
 	// On rentre les différentes dénominations de notre custom post type qui seront affichées dans l'administration
 	$labels = array(
-		'name'                => _x('Mentormarketing_products', 'Post Type General Name', 'twentytwentyone'),
-		'singular_name'       => _x('Mentormarketing', 'Post Type Singular Name', 'twentytwentyone'),
-		'menu_name'           => __('Mentormarketing_product', 'twentytwentyone'),
-		'name_admin_bar'      => __('Mentormarketing_Product', 'twentytwentyone'),
-		'parent_item_colon'   => __('Parent Item:', 'twentytwentyone'),
-		'all_items'           => __('All Products', 'twentytwentyone'),
-		'add_new_item'        => __('Add New Product', 'twentytwentyone'),
-		'add_new'             => __('Add New product', 'twentytwentyone'),
-		'new_item'            => __('New Item', 'twentytwentyone' ),
-		'edit_item'           => __('Edit Item', 'twentytwentyone'),
-		'update_item'         => __('Update Item', 'twentytwentyone'),
-		'view_item'           => __('View Item', 'twentytwentyone'),
-		'search_items'        => __('Search Item', 'twentytwentyone'),
-		'not_found'           => __('Not found', 'twentytwentyone'),
-		'not_found_in_trash'  => __('Not found in Trash', 'twentytwentyone'),
+		'name'                => _x('Mentormarketing_products', 'Post Type General Name', 'twentytwenty'),
+		'singular_name'       => _x('Mentormarketing', 'Post Type Singular Name', 'twentytwenty'),
+		'menu_name'           => __('Mentormarketing_product', 'twentytwenty'),
+		'name_admin_bar'      => __('Mentormarketing_Product', 'twentytwenty'),
+		'parent_item_colon'   => __('Parent Item:', 'twentytwenty'),
+		'all_items'           => __('All Products', 'twentytwenty'),
+		'add_new_item'        => __('Add New Product', 'twentytwenty'),
+		'add_new'             => __('Add New product', 'twentytwenty'),
+		'new_item'            => __('New Item', 'twentytwenty' ),
+		'edit_item'           => __('Edit Item', 'twentytwenty'),
+		'update_item'         => __('Update Item', 'twentytwenty'),
+		'view_item'           => __('View Item', 'twentytwenty'),
+		'search_items'        => __('Search Item', 'twentytwenty'),
+		'not_found'           => __('Not found', 'twentytwenty'),
+		'not_found_in_trash'  => __('Not found in Trash', 'twentytwenty'),
 	);
 	$rewrite = array(
-		'slug'                => _x('Mentormarketing', 'Mentormarketing', 'twentytwentyone'),
+		'slug'                => _x('Mentormarketing', 'Mentormarketing', 'twentytwenty'),
 		'with_front'          => true,
 		'pages'               => true,
 		'feeds'               => false,
 	);
 	$args = array(
-		'label'               => __('Mentormarketing', 'twentytwentyone'),
-		'description'         => __('Mentormarketing', 'twentytwentyone'),
+		'label'               => __('Mentormarketing', 'twentytwenty'),
+		'description'         => __('Mentormarketing', 'twentytwenty'),
 		'labels'              => $labels,
 		'supports'            => array('title', 'editor', 'thumbnail', 'comments', 'revisions', 'custom-fields'),
-		'taxonomies'          => array('Mentormarketing_type'),
 		'rest_controller_class" => "WP_REST_Posts_Controller',
 		'hierarchical'        => false,
 		'public'              => true,
@@ -50,13 +49,13 @@ function wpm_custom_post_type() {
 		'exclude_from_search' => false,
 		'publicly_queryable'  => true,
 		'query_var'           => 'Mentormarketing',
-		'rewrite'             => ['slug' => 'mentormarketing_prod', 'with_front' => true],
+		'rewrite'             =>  $rewrite,
 		'capability_type'     => 'page',
 		'supports' => ['title', 'editor', 'thumbnail'],
         'taxonomies' => ['category', 'post_tag'],
         'show_in_graphql' => false,
 	);
-	register_post_type('property', $args);	
+	register_post_type('Mentormarketing', $args);	
 }
 add_action( 'init', 'wpm_custom_post_type', 10 );
 /**
@@ -65,7 +64,7 @@ add_action( 'init', 'wpm_custom_post_type', 10 );
 function wpc_cpt_in_home($query) {
 	if (! is_admin() && $query->is_main_query()) {
 	  if ($query->is_home) {
-		$query->set('post_type', array('post', 'property'));
+		$query->set('post_type', array('post', 'Mentormarketing'));
 	  }
 	}
   }
